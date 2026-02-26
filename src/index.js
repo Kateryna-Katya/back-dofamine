18f2daec2d0f1aaa5e933800c567b2acf3ae8874
Kateryna-Katya <165307921+Kateryna-Katya@users.noreply.github.com>
2026-02-27 01:33:31 +0200

    add

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { seedAdmin } from './db/seedAdmin.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 // healthcheck
